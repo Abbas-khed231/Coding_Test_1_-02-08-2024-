68c9c15f1eb273870bf9a258407a0c7acca5577e
AbbasKhed_231 <khedabbas@gmail.com>
2024-08-02 16:04:33 +0530

    Coding test 1 solution

---
+++ test_solution_1.py
@@ -0,0 +1,5 @@
+#1. Write a Python program to read a CSV file and display its contents.
+
+import pandas as pd
+df = pd.read_csv("aerofit_treadmill.csv")
+df
---
+++ test_solution_2.py
@@ -0,0 +1,12 @@
+#2. Write a Python function to reverse a string.
+
+def reverse_string(string):
+    print(string[::-1])
+
+string = "My name is Abbas Khed"
+reverse_string(string)
+
+'''
+Output:
+dehK sabbA si eman yM
+'''
